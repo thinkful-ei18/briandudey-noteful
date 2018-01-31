b65e50f110d9a13e224ab41f6039195eab9a0793
Brian Dudey <dudey.brian@gmail.com>
2018-01-31 09:18:16 -0600

    Day Two Commit

--- server.js
+++ server.js
@@ -1,7 +1,85 @@
 'use strict';
 
 const data = require('./db/notes');
+const simDB = require('./db/simDB');
+const notes = simDB.initialize(data);
 
-console.log('hello world!');
+const { PORT } = require('./config');
+const { requestLogger } = require('./request-logger');
 
-// INSERT EXPRESS APP CODE HERE...
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.use(express.json());
+  
+app.use(requestLogger);
+
+app.get('/v1/notes', (req, res, next) => {
+  const { searchTerm } = req.query;
+
+  notes.filter(searchTerm, (err, list) => {
+    if (err) {
+      return next(err);
+    }
+    res.json(list);
+  });
+});
+
+app.get('/v1/notes/:id', (req, res, next) => {
+  let id = req.params.id;
+  notes.find(id, (err, item) => {
+    if (err) {
+      return next(err);
+    }
+    if (item) {
+      res.json(item);
+    }
+  });
+});
+
+app.put('/v1/notes/:id', (req, res, next) => {
+  const id = req.params.id;
+  //=======================Never trust users - validate input===
+  const updateObj = {};
+  const updateFields = ['title', 'content'];
+
+  updateFields.forEach(field => {
+    if (field in req.body) {
+      updateObj[field] = req.body[field];
+    }
+  });
+
+  notes.update(id, updateObj, (err, item) => {
+    if (err) {
+      return next(err);
+    }
+    if (item) {
+      res.json(item);
+    }
+    else {
+      next();
+    }
+  });
+});
+
+app.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  res.status(404).json({ message: 'Not Found' });
+});
+
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: err
+  });
+});
+
+app.listen(PORT, function () {
+  console.info(`Server listening on ${this.address().port}`);
+}).on('error', err => {
+  console.error(err);
+});
